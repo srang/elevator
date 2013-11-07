0403b809e5af4fb6c38084dddde202cf07f4c10d
ptv999 <ptv999duke@yahoo.com>
2013-11-07 15:26:15 -0500

    UnboundedSingleElevator some pseudocode

---
+++ UnboundedSingleBuilding.java
@@ -0,0 +1,20 @@
+public class UnboundedSingleBuilding extends AbstractBuilding{
+
+	public UnboundedSingleBuilding(int numFloors, int numElevators) {
+		super(numFloors, numElevators);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public AbstractElevator CallUp(int fromFloor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AbstractElevator CallDown(int fromFloor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
