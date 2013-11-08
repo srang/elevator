85446cb0f4dfc7c264d68f76c87698d4139cd8c0
Sam Rang <srang2010@gmail.com>
2013-11-07 20:29:28 -0500

    Implementing Rider which extends Thread

--- Rider.java
+++ Rider.java
@@ -1,10 +1,32 @@
 
 public class Rider extends Thread{
 	private int requestedFloor;
-	public Rider(int floor){
-		this.requestedFloor = floor;
+	private int originFloor;
+	private AbstractBuilding myBuilding;
+	public Rider(int desiredFloor, int currentFloor, AbstractBuilding building){
+		this.requestedFloor = desiredFloor;
+		this.originFloor = currentFloor;
+		this.myBuilding = building;
 	}
 	public int getRequestedFloor(){
 		return this.requestedFloor;
+	}
+	@Override
+	public void run() {
+		AbstractElevator myElevator;
+		if(requestedFloor > originFloor) { 
+			myElevator = myBuilding.CallUp(originFloor);
+		} else {
+			myElevator = myBuilding.CallDown(originFloor);
+		}
+		while(!(myElevator.Enter())) {
+			if(requestedFloor > originFloor) { 
+				myElevator = myBuilding.CallUp(originFloor);
+			} else {
+				myElevator = myBuilding.CallDown(originFloor);
+			}
+		} 
+		myElevator.RequestFloor(requestedFloor);
+		myElevator.Exit();
 	}
 }
