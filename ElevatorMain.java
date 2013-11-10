231113a4e45f5c418a8c31b619b100af2dd6b02d
shariqiqbal2810 <sabeeniqbal@wl-10-191-2-241.wireless.duke.edu>
2013-11-09 21:25:39 -0500

    Set up Main Class for Execution in command line

--- ElevatorMain.java
+++ ElevatorMain.java
@@ -4,21 +4,27 @@ public class ElevatorMain {
 	public static void main(String[] args) {
 
 		if(args.length==0) {
-			TestEventBarrier.test();
-			//System.out.println("Not enough arguments");
+			//TestEventBarrier.test();
+			System.out.println("Not enough arguments");
 		} 
-		/*else if(args.length > 2) {
+		else if(args.length > 2) {
 			System.out.println("Too many arguments");
-		} else // known just one arg
-			if(args[1].equals("p1")){
-				TestEventBarrier.test();
-			}
-		if(args[1].equals("p2part1")) {
-			// call the elevator part1
-		} else if(args[1].equals("p2part2")) {
+		} 
+		else if(args[0].equals("p1"))
+		{
+			System.out.println("Event Barrier:");
+			TestEventBarrier.test();
+		}
+		else if(args[0].equals("p2part1")) {
+			System.out.println("Single Unbounded Elevator:");
+		}
+		else if(args[0].equals("p2part2")) {
+			System.out.println("Single Bounded Elevator:");
 			// call the elevator part2
-		} else if(args[1].equals("p2part3")){
+		}
+		else if(args[0].equals("p2part3")){
+			System.out.println("Multiple Bounded Elevators:");
 			// call the elevator part3
-		} */
+		}
 	}
 }
