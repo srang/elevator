e2ad1a409a66961a3b62da98ef0249a700faace9
ptv999 <Duke@wl-10-191-3-121.wireless.duke.edu>
2013-11-09 22:43:59 -0500

    1 liner

--- ElevatorTest.java
+++ ElevatorTest.java
@@ -3,6 +3,7 @@ public class ElevatorTest {
 		SingleBuildingVersion2 myBuilding = new SingleBuildingVersion2(20, 1);
 		Elevator elevator = new Elevator(myBuilding);
 		Rider rider = new Rider(1, 10, 2, myBuilding);
+		rider.start();
 		elevator.start();
 	}
 }
