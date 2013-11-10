a51da3655b4e2d285efe77c4bff1b666d3eeb60e
shariqiqbal2810 <sabeeniqbal@wl-10-191-2-241.wireless.duke.edu>
2013-11-09 20:12:42 -0500

    Test Case for Multiple EventBarriers

--- TestEventBarrier.java
+++ TestEventBarrier.java
@@ -1,16 +1,23 @@
 public class TestEventBarrier {
 	public static void test() {
-		for(int i = 0; i < 15; i++) {
-			EventBarrier evBar = new EventBarrier();
-			System.out.println("Created a new EventBarrier");
-			for(int s = 0; s < 10; s++){
-				Thread t = new helperThread(evBar);
-				t.start();
-				System.out.println("Creating a new Thread " + t.getId());
-			}
-			System.out.println("Call raise from Thread " + Thread.currentThread().getId());
-			evBar.raise();
+		EventBarrier evBar1 = new EventBarrier();
+		EventBarrier evBar2 = new EventBarrier();
+		System.out.println("Created a new EventBarrier");
+		for(int s = 0; s < 5; s++){
+			Thread t = new helperThread(evBar1);
+			t.start();
+			System.out.println("Creating a new Thread: " + t.getId() + " at barrier 1");
 		}
+		for(int s = 0; s < 3; s++){
+			Thread t = new helperThread(evBar2);
+			t.start();
+			System.out.println("Creating a new Thread: " + t.getId() + " at barrier 2");
+		}
+		
+		System.out.println("Barrier 1 call raise from Thread " + Thread.currentThread().getId());
+		evBar2.raise();
+		evBar1.raise();
+		
 	}
 }
 class helperThread extends Thread {
